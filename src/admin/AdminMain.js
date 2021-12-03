9b14a3e3977a82e12864891c79989708e879c653
roshandeorukhkar <roshandeorukhkar@gmail.com>
2021-12-03 14:34:07 +0530

    initial

---
+++ src/admin/AdminMain.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const AdminMain = () => {
+    
+    return (
+        <div>
+            Admin
+        </div>
+    );
+};
+
+export default AdminMain;
